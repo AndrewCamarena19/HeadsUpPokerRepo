c77c6b94a2c4ecce9d0b76865c50f6b0fac10930
Andy <andycamarena@ymail.com>
2017-11-30 12:56:09 -0800

    Implemented reverse sort for Razz and Low Hands

--- app/src/main/java/com/andyisdope/headsuppoker/Hand.java
+++ app/src/main/java/com/andyisdope/headsuppoker/Hand.java
@@ -25,7 +25,13 @@ public class Hand {
 
     public void addCard(Card card) {
         Cards.add(card);
-        Collections.sort(Cards, CardComparator);
+        Collections.sort(Cards, CardComparatorHigh);
+    }
+
+    public void addCardLow(Card card)
+    {
+        Cards.add(card);
+        Collections.sort(Cards, CardComparatorLow);
     }
 
     public Card getCard(int index) {
@@ -47,10 +53,17 @@ public class Hand {
         }
     }
 
-    private Comparator<Card> CardComparator = new Comparator<Card>() {
+    private Comparator<Card> CardComparatorHigh = new Comparator<Card>() {
         @Override
         public int compare(Card card, Card t1) {
             return t1.getRank().compareTo(card.getRank());
+        }
+    };
+
+    private Comparator<Card> CardComparatorLow = new Comparator<Card>() {
+        @Override
+        public int compare(Card card, Card t1) {
+            return card.getRank().compareTo(t1.getRank());
         }
     };
 }
