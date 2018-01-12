6d6d933b938d1e203c2f508e8f81fc66610aa532
Andy <andycamarena@ymail.com>
2018-01-12 15:50:49 -0800

    Razz Table Layout and Kotlin Activity

---
+++ app/src/main/java/com/andyisdope/headsuppoker/RazzTable.kt
@@ -0,0 +1,12 @@
+package com.andyisdope.headsuppoker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RazzTable : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_razz_table)
+    }
+}
